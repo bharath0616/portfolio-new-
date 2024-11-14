3110becc7cd779174b4f939534cd94716059b1ce
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2024-11-14 23:40:59 +0530

    changes

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import resume from '../../Assets/Resume.pdf'
 import "./resume.css";
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import { pdfjs, Document, Page } from "react-pdf";
+import resumee from '../../Assets/resume.png'
 
 function Resume() {
   const [width, setWidth] = useState(1200);
@@ -32,9 +31,7 @@ function Resume() {
           </Button>
         </Row>
         <Row className="resume">
-          <Document file={resume} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
+          <img src={resumee}/>
         </Row>
         <Row className="download-row" style={{ justifyContent: "center", paddingTop: "20px" }}>
           <Button
